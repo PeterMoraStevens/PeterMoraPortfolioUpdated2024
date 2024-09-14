f5b62f4ed5a6d98963d09d162d78b0939652dd93
Peter Mora-Stevens <petermora-stevens@Peters-MacBook-Air.local>
2024-09-13 17:08:33 -0700

    updated dropdown menu

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,6 +1,7 @@
 import { useState, useRef, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Button } from "./ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import {
   BriefcaseBusiness,
   Home,
@@ -8,43 +9,16 @@ import {
   Newspaper,
   SmileIcon,
 } from "lucide-react";
-import { motion } from "framer-motion";
 import Brutal1 from "../assets/Brutalist 53.svg";
 import Brutal2 from "../assets/Brutalist 59.svg";
 import Brutal3 from "../assets/Brutalist 62.png";
 import Brutal4 from "../assets/Brutalist 86.svg";
 
 const Navbar = () => {
-  const [isShown, setIsShown] = useState(false);
   const [screenWidth, setScreenWidth] = useState(window.screen.width);
-  const menuRef = useRef<HTMLDivElement>(null);
-  const location = useLocation();
 
   const screenSize = () => {
     setScreenWidth(window.screen.width);
-  };
-
-  useEffect(() => {
-    setIsShown(false);
-  }, [location]);
-
-  const variants = {
-    open: {
-      height: menuRef.current ? menuRef.current.scrollHeight : 0,
-      opacity: 1,
-      transition: {
-        height: { duration: 0.5, ease: "easeOut" },
-        opacity: { duration: 0.3, ease: "easeOut" },
-      },
-    },
-    closed: {
-      height: 0,
-      opacity: 0,
-      transition: {
-        height: { duration: 0.5, ease: "easeOut" },
-        opacity: { duration: 0.3, ease: "easeOut" },
-      },
-    },
   };
 
   useEffect(() => {
@@ -110,39 +84,48 @@ const Navbar = () => {
   const NavbarPhone = () => {
     return (
       <div className="font-spaceGrotesk p-6 px-12 bg-bg border-darkBorder border-b-4 flex items-center">
-        <Button onClick={() => setIsShown(!isShown)} className="p-2 mr-2">
-          <Menu />
-        </Button>
-
-        <motion.div
-          initial="closed"
-          animate={isShown ? "open" : "closed"}
-          variants={variants}
-          className="overflow-hidden bg-bg rounded-md justify-end self-end"
-        >
-          <div ref={menuRef} className="flex flex-col p-2">
-            <Link to={"/"} className="mx-1 m-1">
-              <Button>
-                <Home className="mr-2" /> Home
-              </Button>
-            </Link>
-            <Link to={"/about"} className="mx-1 m-1">
-              <Button>
-                <SmileIcon className="mr-2" /> About
-              </Button>
-            </Link>
-            <Link to={"/portfolio"} className="mx-1 m-1">
-              <Button>
-                <BriefcaseBusiness className="mr-2" /> Portfolio
-              </Button>
-            </Link>
-            <Link to={"/blog"} className="mx-1 m-1">
-              <Button>
-                <Newspaper className="mr-2" /> Blog
-              </Button>
-            </Link>
-          </div>
-        </motion.div>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button variant="noShadow" className="p-2 mr-2">
+              <Menu />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-48 dark:text-text ml-4">
+            <div className="grid gap-4">
+              <div className="space-y-2"></div>
+              <div className="grid gap-2">
+                <div className="grid grid-cols-3 items-center gap-4">
+                  <Link to={"/"} className="mx-1 m-1">
+                    <Button>
+                      <Home className="mr-2" /> Home
+                    </Button>
+                  </Link>
+                </div>
+                <div className="grid grid-cols-3 items-center gap-4">
+                  <Link to={"/about"} className="mx-1 m-1">
+                    <Button>
+                      <SmileIcon className="mr-2" /> About
+                    </Button>
+                  </Link>
+                </div>
+                <div className="grid grid-cols-3 items-center gap-4">
+                  <Link to={"/portfolio"} className="mx-1 m-1">
+                    <Button>
+                      <BriefcaseBusiness className="mr-2" /> Portfolio
+                    </Button>
+                  </Link>
+                </div>
+                <div className="grid grid-cols-3 items-center gap-4">
+                  <Link to={"/blog"} className="mx-1 m-1">
+                    <Button>
+                      <Newspaper className="mr-2" /> Blog
+                    </Button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </PopoverContent>
+        </Popover>
 
         {/* Move this div to the right */}
         <div className="flex items-center ml-auto">
